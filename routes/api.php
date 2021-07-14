569d4a606d0771318edbe6a99e9d788670ed8aba
FullstackDev7 <antongapeev@outlook.com>
2021-07-14 09:09:42 +0300

    update

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/expenses', 'ExpenseController@all')->name('expenses.all');
+Route::post('/expenses', 'ExpenseController@store')->name('expenses.store');
+Route::get('/expenses/{expenses}', 'ExpenseController@show')->name('expenses.show');
+Route::put('/expenses/{expenses}', 'ExpenseController@update')->name('expenses.update');
+Route::delete('/expenses/{expenses}', 'ExpenseController@destroy')->name('expenses.destroy');
